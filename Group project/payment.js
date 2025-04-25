c95eea03d610860891775b16801f228e26b0c3d2
Aceee98 <dillanj.college@gmail.com>
2025-04-25 10:09:55 +0100

    Added payment overlay

--- Group project/payment.js
+++ Group project/payment.js
@@ -1,2 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    button = 
+document.getElementById('submit-button').addEventListener('click', function() {
+    const overlay = document.getElementById('success-overlay');
+    if (overlay) {
+        overlay.style.display = 'flex';
+    }
+});
